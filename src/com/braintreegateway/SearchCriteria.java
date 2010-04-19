023e59407b5d6dd402d754ac2e657529c645bb0c
drew and paul <code@getbraintree.com>
2010-04-19 15:17:26 -0500

    XML escape subscription searches.

--- src/com/braintreegateway/SearchCriteria.java
+++ src/com/braintreegateway/SearchCriteria.java
@@ -2,22 +2,32 @@ package com.braintreegateway;
 
 import java.util.List;
 
-public class SearchCriteria {
+public class SearchCriteria extends Request {
     private String xml;
 
     public SearchCriteria(String type, String value) {
-        this.xml = String.format("<%s>%s</%s>", type, value, type);
+        this.xml = buildXMLElement(type, value);
     }
-    
+
     public SearchCriteria(List<?> items) {
         StringBuilder builder = new StringBuilder();
         for (Object item : items) {
-            builder.append(String.format("<item>%s</item>", item.toString()));
+            builder.append(buildXMLElement("item", item.toString()));
         }
         this.xml = builder.toString();
     }
 
     public String toXML() {
         return this.xml;
+    }
+
+    @Override
+    public String toQueryString(String parent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toQueryString() {
+        throw new UnsupportedOperationException();
     }
 }
