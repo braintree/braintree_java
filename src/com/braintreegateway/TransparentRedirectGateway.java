d63a04c4043517315f3e436bb5048e069ae0a6c9
drew and patrick <code@getbraintree.com>
2010-06-18 10:12:13 -0500

    [#1827] - Raise IllegalArgumentException if trying to confirm the wrong class

--- src/com/braintreegateway/TransparentRedirectGateway.java
+++ src/com/braintreegateway/TransparentRedirectGateway.java
@@ -2,6 +2,7 @@ package com.braintreegateway;
 
 import com.braintreegateway.util.Http;
 import com.braintreegateway.util.NodeWrapper;
+import com.braintreegateway.util.StringUtils;
 import com.braintreegateway.util.TrUtil;
 
 public class TransparentRedirectGateway {
@@ -29,9 +30,17 @@ public class TransparentRedirectGateway {
         return confirmTr(Transaction.class, queryString);
     }
     
+    public String trData(Request trData, String redirectURL) {
+        return new TrUtil(configuration).buildTrData(trData, redirectURL);
+    }
+    
     private <T> Result<T> confirmTr(Class<T> klass, String queryString) {
         TransparentRedirectRequest trRequest = new TransparentRedirectRequest(configuration, queryString);
         NodeWrapper node = http.post("/transparent_redirect_requests/" + trRequest.getId() + "/confirm", trRequest);
+        if (!node.getElementName().equals(StringUtils.classToXMLName(klass))) {
+            throw new IllegalArgumentException("You attemped to confirm a " + StringUtils.classToXMLName(klass) + ", but received a " + node.getElementName() + ".");
+        }
+        
         return new Result<T>(node, klass);
     }
 }
---
+++ test/com/braintreegateway/TransparentRedirectTest.java
@@ -0,0 +1,129 @@
+package com.braintreegateway;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.braintreegateway.SandboxValues.CreditCardNumber;
+import com.braintreegateway.SandboxValues.TransactionAmount;
+
+public class TransparentRedirectTest {
+    private BraintreeGateway gateway;
+    
+    @Before
+    public void createGateway() {
+        this.gateway = new BraintreeGateway(Environment.DEVELOPMENT, "integration_merchant_id", "integration_public_key", "integration_private_key");
+    }
+    
+    @Test
+    public void createTransactionFromTransparentRedirect() {
+        TransactionRequest request = new TransactionRequest().
+            amount(TransactionAmount.AUTHORIZE.amount).
+            creditCard().
+                number(CreditCardNumber.VISA.number).
+                expirationDate("05/2009").
+                done().
+            options().
+                storeInVault(true).
+                done();
+
+        TransactionRequest trParams = new TransactionRequest().
+            type(Transaction.Type.SALE);
+    
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, request, gateway.transparentRedirect().url());
+        Result<Transaction> result = gateway.transparentRedirect().confirmTransaction(queryString);
+        Assert.assertTrue(result.isSuccess());
+        Assert.assertEquals(CreditCardNumber.VISA.number.substring(0, 6), result.getTarget().getCreditCard().getBin());
+        Assert.assertEquals(TransactionAmount.AUTHORIZE.amount, result.getTarget().getAmount());
+    }
+    
+    @Test
+    public void createCustomerFromTransparentRedirect() {
+        CustomerRequest request = new CustomerRequest().firstName("John");
+        CustomerRequest trParams = new CustomerRequest().lastName("Doe");
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, request, gateway.transparentRedirect().url());
+        
+        Result<Customer> result = gateway.transparentRedirect().confirmCustomer(queryString);
+        
+        Assert.assertTrue(result.isSuccess());
+        Assert.assertEquals("John", result.getTarget().getFirstName());
+        Assert.assertEquals("Doe", result.getTarget().getLastName());
+    }
+    
+    @Test
+    public void updateCustomerFromTransparentRedirect() {
+        CustomerRequest request = new CustomerRequest().
+            firstName("John").
+            lastName("Doe");
+        Customer customer = gateway.customer().create(request).getTarget();
+        
+        CustomerRequest updateRequest = new CustomerRequest().firstName("Jane");
+        CustomerRequest trParams = new CustomerRequest().customerId(customer.getId()).lastName("Dough");
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, updateRequest, gateway.transparentRedirect().url());
+        
+        Result<Customer> result = gateway.transparentRedirect().confirmCustomer(queryString);
+        
+        Assert.assertTrue(result.isSuccess());
+        Customer updatedCustomer = gateway.customer().find(customer.getId());
+        Assert.assertEquals("Jane", updatedCustomer.getFirstName());
+        Assert.assertEquals("Dough", updatedCustomer.getLastName());
+    }
+    
+    @Test
+    public void createCreditCardFromTransparentRedirect() {
+        Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
+        CreditCardRequest request = new CreditCardRequest();
+        CreditCardRequest trParams = new CreditCardRequest().
+            customerId(customer.getId()).
+            number("4111111111111111").
+            expirationDate("10/10");
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, request, gateway.transparentRedirect().url());
+        
+        Result<CreditCard> result = gateway.transparentRedirect().confirmCreditCard(queryString);
+        
+        Assert.assertTrue(result.isSuccess());
+        Assert.assertEquals("411111", result.getTarget().getBin());
+        Assert.assertEquals("1111", result.getTarget().getLast4());
+        Assert.assertEquals("10/2010", result.getTarget().getExpirationDate());
+    }
+    
+    @Test
+    public void updateCreditCardFromTransparentRedirect() {
+        Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
+        CreditCardRequest request = new CreditCardRequest().
+            customerId(customer.getId()).
+            number("5105105105105100").
+            expirationDate("05/12");
+        CreditCard card = gateway.creditCard().create(request).getTarget();
+        
+        CreditCardRequest updateRequest = new CreditCardRequest();
+        CreditCardRequest trParams = new CreditCardRequest().
+            paymentMethodToken(card.getToken()).
+            number("4111111111111111").
+            expirationDate("10/10");
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, updateRequest, gateway.transparentRedirect().url());
+        
+        Result<CreditCard> result = gateway.transparentRedirect().confirmCreditCard(queryString);
+        
+        Assert.assertTrue(result.isSuccess());
+        CreditCard updatedCreditCard = gateway.creditCard().find(card.getToken());
+        Assert.assertEquals("411111", updatedCreditCard.getBin());
+        Assert.assertEquals("1111", updatedCreditCard.getLast4());
+        Assert.assertEquals("10/2010", updatedCreditCard.getExpirationDate());
+    }
+    
+    @Test
+    public void errorRaisedWhenConfirmingIncorrectResource() {
+        CustomerRequest request = new CustomerRequest().firstName("John");
+        CustomerRequest trParams = new CustomerRequest().lastName("Doe");
+        String queryString = TestHelper.simulateFormPostForTR(gateway, trParams, request, gateway.transparentRedirect().url());
+        
+        try {
+            gateway.transparentRedirect().confirmTransaction(queryString);
+            Assert.assertTrue(false);
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("You attemped to confirm a transaction, but received a customer.", e.getMessage());
+        }
+    }
+}
