12ddde43a98345a916537c8401aebfe144fcdabb
brian and micah <code@getbraintree.com>
2011-09-16 13:54:04 -0500

    remove old xpath-based NodeWrapper classes/tests

--- src/com/braintreegateway/util/XmlNodeWrapperFactory.java
+++
@@ -1,7 +0,0 @@
-package com.braintreegateway.util;
-
-public class XmlNodeWrapperFactory implements NodeWrapperFactory {
-    public NodeWrapper create(String xml) {
-        return new XmlNodeWrapper(xml);
-    }
-}
