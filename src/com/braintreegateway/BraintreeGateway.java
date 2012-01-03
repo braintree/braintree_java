4c038b08b7b512445989e70004d3d3fc09b21a13
cory and pilat <code@getbraintree.com>
2012-01-03 16:40:00 -0600

    Bumped version and updated changelog

--- src/com/braintreegateway/BraintreeGateway.java
+++ src/com/braintreegateway/BraintreeGateway.java
@@ -46,7 +46,7 @@ import com.braintreegateway.util.TrUtil;
  */
 public class BraintreeGateway {
 
-    public static final String VERSION = "2.13.1";
+    public static final String VERSION = "2.13.2";
 
     private Configuration configuration;
     private Environment environment;
