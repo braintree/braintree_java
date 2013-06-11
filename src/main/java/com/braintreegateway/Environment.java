7fcf63ba4e298712e34611fa38946f05350395e3
Braintree <code@getbraintree.com>
2013-06-11 14:44:52 -0500

    Make Environment a class so Joerg can instantiate it with QA values

--- src/main/java/com/braintreegateway/Environment.java
+++ src/main/java/com/braintreegateway/Environment.java
@@ -5,44 +5,21 @@ import java.util.Arrays;
 /**
  * Indicates the environment of the Braintree Gateway with which to interact.
  */
-public enum Environment {
+public class Environment {
     /** For Braintree internal development. */
-    DEVELOPMENT(developmentHost() + ":" + developmentPort(), developmentCertificates()),
+    public static final Environment DEVELOPMENT = new Environment("http://localhost:3000", new String[] {});
 
     /** For production. */
-    PRODUCTION("https://www.braintreegateway.com:443", new String[] {"ssl/www_braintreegateway_com.ca.der", "ssl/securetrust.ca.der"}),
+    public static final Environment PRODUCTION = new Environment("https://www.braintreegateway.com:443", new String[] {"ssl/www_braintreegateway_com.ca.der", "ssl/securetrust.ca.der"});
 
     /** For merchant's to use during their development and testing. */
-    SANDBOX("https://sandbox.braintreegateway.com:443", new String[] {"ssl/sandbox-godaddy-root.ca.der", "ssl/sandbox_braintreegateway_com.ca.der", "ssl/sandbox-godaddy-intermediate.ca.der"});
+    public static final Environment SANDBOX = new Environment("https://sandbox.braintreegateway.com:443", new String[] {"ssl/sandbox-godaddy-root.ca.der", "ssl/sandbox_braintreegateway_com.ca.der", "ssl/sandbox-godaddy-intermediate.ca.der"});
 
     public final String baseURL;
     public final String[] certificateFilenames;
 
-    private Environment(String baseURL, String[] certificateFilenames) {
+    public Environment(String baseURL, String[] certificateFilenames) {
         this.baseURL = baseURL;
         this.certificateFilenames = Arrays.copyOf(certificateFilenames, certificateFilenames.length);
-    }
-
-    private static String developmentPort() {
-        if (System.getenv().get("GATEWAY_PORT") != null) {
-            return System.getenv().get("GATEWAY_PORT");
-        } else if (System.getProperty("com.braintreegateway.port") != null) {
-            return System.getProperty("com.braintreegateway.port");
-        } else {
-            return "3000";
-        }
-    }
-
-    private static String developmentHost() {
-        return System.getProperty("com.braintreegateway.host") == null ? "http://localhost" : System.getProperty("com.braintreegateway.host");
-    }
-
-    private static String[] developmentCertificates() {
-        if (System.getProperty("com.braintreegateway.certificates") == null) {
-            return new String[] {};
-        } else {
-            String certs = System.getProperty("com.braintreegateway.certificates");
-            return certs.split(",");
-        }
     }
 }
