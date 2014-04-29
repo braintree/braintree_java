7593b8a2258a69b91f2e169cd93466067fe59f67
danquan and jakesandlund <code@getbraintree.com>
2014-04-29 20:22:12 +0000

    Add tests for secure compare

---
+++ src/test/java/com/braintreegateway/util/CryptoTest.java
@@ -0,0 +1,25 @@
+package com.braintreegateway.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+public class CryptoTest {
+
+    @Test
+    public void secureCompareWithEqualStrings() {
+        assertTrue(new Crypto().secureCompare("a_string", "a_string"));
+    }
+
+    @Test
+    public void secureCompareWithNotEqualStrings() {
+        assertFalse(new Crypto().secureCompare("a_string", "a_strong"));
+    }
+
+    @Test
+    public void secureCompareWithLongerNotEqualStrings() {
+        assertFalse(new Crypto().secureCompare("a_string", "a_string_that_is_longer"));
+    }
+}
