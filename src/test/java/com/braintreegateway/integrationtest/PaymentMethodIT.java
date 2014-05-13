b4bbd632b0d3f4a7775a114e20100f2e0585bf13
tony <code@getbraintree.com>
2014-05-13 22:51:38 +0000

    altpay #508 - payment method delete and find

--- src/test/java/com/braintreegateway/integrationtest/PaymentMethodIT.java
+++ src/test/java/com/braintreegateway/integrationtest/PaymentMethodIT.java
@@ -2,6 +2,7 @@ package com.braintreegateway.integrationtest;
 
 import com.braintreegateway.*;
 import com.braintreegateway.testhelpers.TestHelper;
+import com.braintreegateway.exceptions.NotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,11 +19,11 @@ public class PaymentMethodIT {
 
     @Test
     public void createPayPalAccountWithNonce() {
-        String nonce = TestHelper.generateFuturePaymentPayPalNonce(gateway);
         Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
         assertTrue(customerResult.isSuccess());
         Customer customer = customerResult.getTarget();
 
+        String nonce = TestHelper.generateFuturePaymentPayPalNonce(gateway);
         PaymentMethodRequest request = new PaymentMethodRequest().
             customerId(customer.getId()).
             paymentMethodNonce(nonce);
@@ -35,6 +36,26 @@ public class PaymentMethodIT {
 
         PayPalAccount paypalAccount = (PayPalAccount) paymentMethod;
         assertNotNull(paypalAccount.getEmail());
+    }
+
+    @Test
+    public void createPayPalAccountWithOneTimeNonceFails() {
+        Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
+        assertTrue(customerResult.isSuccess());
+        Customer customer = customerResult.getTarget();
+
+        String nonce = TestHelper.generateOneTimePayPalNonce(gateway);
+        PaymentMethodRequest request = new PaymentMethodRequest().
+            customerId(customer.getId()).
+            paymentMethodNonce(nonce);
+
+        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+
+        assertFalse(result.isSuccess());
+        assertEquals(
+                ValidationErrorCode.PAYPAL_ACCOUNT_CANNOT_VAULT_ONE_TIME_USE_PAYPAL_ACCOUNT,
+                result.getErrors().forObject("paypalAccount").onField("base").get(0).getCode()
+                );
     }
 
     @Test
@@ -56,5 +77,114 @@ public class PaymentMethodIT {
 
         CreditCard creditCard = (CreditCard) paymentMethod;
         assertEquals("1111", creditCard.getLast4());
+    }
+
+    @Test
+    public void deletePayPalAccount() {
+        Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
+        assertTrue(customerResult.isSuccess());
+        Customer customer = customerResult.getTarget();
+
+        String nonce = TestHelper.generateFuturePaymentPayPalNonce(gateway);
+        PaymentMethodRequest request = new PaymentMethodRequest().
+            customerId(customer.getId()).
+            paymentMethodNonce(nonce);
+
+        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+
+        assertTrue(result.isSuccess());
+        PaymentMethod paymentMethod = result.getTarget();
+
+        Result<? extends PaymentMethod> deleteResult = gateway.paymentMethod().delete(paymentMethod.getToken());
+        assertTrue(deleteResult.isSuccess());
+    }
+
+    @Test
+    public void deleteCreditCard() {
+        Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
+        assertTrue(customerResult.isSuccess());
+        Customer customer = customerResult.getTarget();
+
+        String nonce = TestHelper.generateUnlockedNonce(gateway, null, SandboxValues.CreditCardNumber.VISA.number);
+        PaymentMethodRequest request = new PaymentMethodRequest().
+            customerId(customer.getId()).
+            paymentMethodNonce(nonce);
+
+        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+
+        assertTrue(result.isSuccess());
+        PaymentMethod paymentMethod = result.getTarget();
+
+        Result<? extends PaymentMethod> deleteResult = gateway.paymentMethod().delete(paymentMethod.getToken());
+        assertTrue(deleteResult.isSuccess());
+    }
+
+    @Test
+    public void deleteMissingRaisesNotFoundError() {
+        try {
+            gateway.paymentMethod().delete("missing");
+            fail("Should throw NotFoundException");
+        } catch (NotFoundException e) {
+        }
+    }
+
+    @Test
+    public void findPayPalAccount() {
+        Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
+        assertTrue(customerResult.isSuccess());
+        Customer customer = customerResult.getTarget();
+
+        String nonce = TestHelper.generateFuturePaymentPayPalNonce(gateway);
+        PaymentMethodRequest request = new PaymentMethodRequest().
+            customerId(customer.getId()).
+            paymentMethodNonce(nonce);
+
+        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+
+        assertTrue(result.isSuccess());
+        PaymentMethod paymentMethod = result.getTarget();
+
+        PaymentMethod found = gateway.paymentMethod().find(paymentMethod.getToken());
+        assertNotNull(found);
+        assertTrue(found instanceof PayPalAccount);
+    }
+
+    @Test
+    public void findCreditCard() {
+        Result<Customer> customerResult = gateway.customer().create(new CustomerRequest());
+        assertTrue(customerResult.isSuccess());
+        Customer customer = customerResult.getTarget();
+
+        String nonce = TestHelper.generateUnlockedNonce(gateway, null, SandboxValues.CreditCardNumber.VISA.number);
+        PaymentMethodRequest request = new PaymentMethodRequest().
+            customerId(customer.getId()).
+            paymentMethodNonce(nonce);
+
+        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+
+        assertTrue(result.isSuccess());
+        PaymentMethod paymentMethod = result.getTarget();
+
+        PaymentMethod found = gateway.paymentMethod().find(paymentMethod.getToken());
+        assertNotNull(found);
+        assertTrue(found instanceof CreditCard);
+    }
+
+    @Test
+    public void findBlankRaisesNotFoundError() {
+        try {
+            gateway.paymentMethod().find("");
+            fail("Should throw NotFoundException");
+        } catch (NotFoundException e) {
+        }
+    }
+
+    @Test
+    public void findMissingRaisesNotFoundError() {
+        try {
+            gateway.paymentMethod().find("missing");
+            fail("Should throw NotFoundException");
+        } catch (NotFoundException e) {
+        }
     }
 }
