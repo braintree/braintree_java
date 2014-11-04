7f6fe07bc80b205443d88c478c060813070aa331
gary <code@getbraintree.com>
2014-11-04 01:19:52 +0000

    sdk rrKIFBav - Constantize apple pay card key

--- src/test/java/com/braintreegateway/integrationtest/TransactionIT.java
+++ src/test/java/com/braintreegateway/integrationtest/TransactionIT.java
@@ -617,7 +617,7 @@ public class TransactionIT implements MerchantAccountTestConstants {
         assertTrue(result.isSuccess());
         Transaction transaction = result.getTarget();
 
-        assertEquals("apple_pay_card", transaction.getPaymentInstrumentType());
+        assertEquals(PaymentInstrumentType.APPLE_PAY_CARD, transaction.getPaymentInstrumentType());
         assertNotNull(transaction.getApplePayDetails());
         assertNotNull(transaction.getApplePayDetails().getCardType());
         assertNotNull(transaction.getApplePayDetails().getExpirationMonth());
