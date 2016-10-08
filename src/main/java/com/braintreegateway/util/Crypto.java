f67cde6db9860490ed7b74de1b486744f6d6d952
Amir Ali Omidi <amir@aaomidi.com>
2016-10-08 03:40:00 -0400

    Update Crypto.java
    Unused imports

--- src/main/java/com/braintreegateway/util/Crypto.java
+++ src/main/java/com/braintreegateway/util/Crypto.java
@@ -1,11 +1,5 @@
 package com.braintreegateway.util;
 
-import com.braintreegateway.org.apache.commons.codec.binary.Hex;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.MessageDigest;
-
 public class Crypto {
     public Boolean secureCompare(String left, String right) {
         if (left == null || right == null || (left.length() != right.length())) {
