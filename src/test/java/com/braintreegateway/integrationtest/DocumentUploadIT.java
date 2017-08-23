86c29f14783d52f19eefec5baf744cd06371dedb
Quinn Neumiiller <quinn@getbraintree.com>
2017-08-23 21:24:53 +0000

    BTCD-58 Add missing tests

--- src/test/java/com/braintreegateway/integrationtest/DocumentUploadIT.java
+++ src/test/java/com/braintreegateway/integrationtest/DocumentUploadIT.java
@@ -6,11 +6,18 @@ import java.net.URL;
 import com.braintreegateway.DocumentUpload;
 import com.braintreegateway.DocumentUploadRequest;
 import com.braintreegateway.Result;
+import com.braintreegateway.ValidationError;
+import com.braintreegateway.ValidationErrorCode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class DocumentUploadIT extends IntegrationTest {
 
@@ -31,5 +38,60 @@ public class DocumentUploadIT extends IntegrationTest {
     assertEquals("image/png", documentUpload.getContentType());
     assertEquals("bt_logo.png", documentUpload.getName());
     assertEquals(2443, documentUpload.getSize());
+  }
+
+  @Test
+  public void createReturnsErrorWithUnsupportedFileType() {
+    URL fileToUpload = getClass().getClassLoader().getResource("fixtures/gif_extension_bt_logo.gif");
+    DocumentUploadRequest uploadRequest = new DocumentUploadRequest(DocumentUpload.Kind.EVIDENCE_DOCUMENT,
+        new File(fileToUpload.getFile()));
+
+    Result<DocumentUpload> uploadResult = gateway.documentUpload().create(uploadRequest);
+    ValidationError error = uploadResult.getErrors()
+      .forObject("documentUpload")
+      .getAllValidationErrors()
+      .get(0);
+
+    assertEquals(ValidationErrorCode.DOCUMENT_UPLOAD_FILE_TYPE_IS_INVALID, error.getCode());
+  }
+
+  @Test
+  public void createReturnsErrorWithMalformedFile() {
+    URL fileToUpload = getClass().getClassLoader().getResource("fixtures/malformed_pdf.pdf");
+    DocumentUploadRequest uploadRequest = new DocumentUploadRequest(DocumentUpload.Kind.EVIDENCE_DOCUMENT,
+        new File(fileToUpload.getFile()));
+
+    Result<DocumentUpload> uploadResult = gateway.documentUpload().create(uploadRequest);
+    ValidationError error = uploadResult.getErrors()
+      .forObject("documentUpload")
+      .getAllValidationErrors()
+      .get(0);
+
+    assertEquals(ValidationErrorCode.DOCUMENT_UPLOAD_FILE_IS_MALFORMED_OR_ENCRYPTED, error.getCode());
+  }
+
+  @Test
+  public void createReturnsErrorWhenFileIsOver4Mb() {
+    try {
+      PrintWriter writer = new PrintWriter("./src/test/resources/fixtures/large_file.pdf");
+      for (int i = 0; i <= 1048577; i++) {
+        writer.print("aaaa");
+      }
+      writer.close();
+    } catch (FileNotFoundException e) {
+      fail("Cannot open large_file.png to write");
+    }
+
+    URL fileToUpload = getClass().getClassLoader().getResource("fixtures/large_file.pdf");
+    DocumentUploadRequest uploadRequest = new DocumentUploadRequest(DocumentUpload.Kind.EVIDENCE_DOCUMENT,
+        new File(fileToUpload.getFile()));
+
+    Result<DocumentUpload> uploadResult = gateway.documentUpload().create(uploadRequest);
+    ValidationError error = uploadResult.getErrors()
+      .forObject("documentUpload")
+      .getAllValidationErrors()
+      .get(0);
+
+    assertEquals(ValidationErrorCode.DOCUMENT_UPLOAD_FILE_IS_TOO_LARGE, error.getCode());
   }
 }
