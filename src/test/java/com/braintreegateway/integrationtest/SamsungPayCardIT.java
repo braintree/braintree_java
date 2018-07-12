5a889dea7b7d0d3e4cbb7cffaebb52b1ceb5b33e
Bradley Bain <bradley.bain@getbraintree.com>
2018-07-12 23:14:18 +0000

    Test for cardholderName and billingAddress

--- src/test/java/com/braintreegateway/integrationtest/SamsungPayCardIT.java
+++ src/test/java/com/braintreegateway/integrationtest/SamsungPayCardIT.java
@@ -1,136 +1,159 @@
 package com.braintreegateway.integrationtest;
 
-import com.braintreegateway.*;
-import com.braintreegateway.exceptions.ForgedQueryStringException;
-import com.braintreegateway.exceptions.NotFoundException;
-import com.braintreegateway.exceptions.DownForMaintenanceException;
+import java.math.BigDecimal;
+
+import com.braintreegateway.Address;
+import com.braintreegateway.AddressRequest;
+import com.braintreegateway.Customer;
+import com.braintreegateway.CustomerRequest;
+import com.braintreegateway.PaymentInstrumentType;
+import com.braintreegateway.PaymentMethod;
+import com.braintreegateway.PaymentMethodRequest;
+import com.braintreegateway.ResourceCollection;
+import com.braintreegateway.Result;
+import com.braintreegateway.SamsungPayCard;
+import com.braintreegateway.SamsungPayCardDetails;
+import com.braintreegateway.SandboxValues;
+import com.braintreegateway.Transaction;
+import com.braintreegateway.TransactionRequest;
+import com.braintreegateway.TransactionSearchRequest;
 import com.braintreegateway.test.Nonce;
+
 import org.junit.Test;
 
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.util.*;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class SamsungPayCardIT extends IntegrationTest {
 
-    @Test
-    public void createWithPaymentMethodNonce() {
-        Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
-        PaymentMethodRequest request = new PaymentMethodRequest().
-            customerId(customer.getId()).
-            paymentMethodNonce(Nonce.SamsungPayVisa);
+  @Test
+  public void createWithPaymentMethodNonce() {
+    Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
+    Address billingAddress = gateway.address().create(
+      customer.getId(),
+      new AddressRequest()
+        .streetAddress("Samsung Pay Merchant Address")
+    ).getTarget();
 
-        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
-        assertTrue(result.isSuccess());
+    PaymentMethodRequest request = new PaymentMethodRequest().
+      customerId(customer.getId()).
+      paymentMethodNonce(Nonce.SamsungPayVisa).
+      cardholderName("Samsung Pay Merchant Name").
+      billingAddressId(billingAddress.getId());
 
-        SamsungPayCard samsungPayCard = (SamsungPayCard) result.getTarget();
-        assertNotNull(samsungPayCard.getBin());
-        assertNotNull(samsungPayCard.getCardType());
-        assertNotNull(samsungPayCard.getCardholderName());
-        assertNotNull(samsungPayCard.getCommercial());
-        assertNotNull(samsungPayCard.getCountryOfIssuance());
-        assertNotNull(samsungPayCard.getCreatedAt());
-        assertNotNull(samsungPayCard.getCustomerId());
-        assertNotNull(samsungPayCard.getCustomerLocation());
-        assertNotNull(samsungPayCard.getDebit());
-        assertNotNull(samsungPayCard.isDefault());
-        assertNotNull(samsungPayCard.getDurbinRegulated());
-        assertNotNull(samsungPayCard.getExpirationDate());
-        assertNotNull(samsungPayCard.getExpirationMonth());
-        assertNotNull(samsungPayCard.getExpirationYear());
-        assertNotNull(samsungPayCard.isExpired());
-        assertNotNull(samsungPayCard.getHealthcare());
-        assertNotNull(samsungPayCard.getImageUrl());
-        assertNotNull(samsungPayCard.getIssuingBank());
-        assertNotNull(samsungPayCard.getLast4());
-        assertNotNull(samsungPayCard.getMaskedNumber());
-        assertNotNull(samsungPayCard.getPayroll());
-        assertNotNull(samsungPayCard.getPrepaid());
-        assertNotNull(samsungPayCard.getProductId());
-        assertNotNull(samsungPayCard.getSubscriptions());
-        assertNotNull(samsungPayCard.getToken());
-        assertNotNull(samsungPayCard.getUniqueNumberIdentifier());
-        assertNotNull(samsungPayCard.getUpdatedAt());
+    Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+    assertTrue(result.isSuccess());
 
-        SamsungPayCard foundSamsungPayCard = (SamsungPayCard) gateway.paymentMethod().find(samsungPayCard.getToken());
-        assertEquals(samsungPayCard.getToken(), foundSamsungPayCard.getToken());
-    }
+    SamsungPayCard samsungPayCard = (SamsungPayCard) result.getTarget();
+    assertNotNull(samsungPayCard.getBillingAddress());
+    assertNotNull(samsungPayCard.getBin());
+    assertNotNull(samsungPayCard.getCardType());
+    assertNotNull(samsungPayCard.getCardholderName());
+    assertNotNull(samsungPayCard.getCommercial());
+    assertNotNull(samsungPayCard.getCountryOfIssuance());
+    assertNotNull(samsungPayCard.getCreatedAt());
+    assertNotNull(samsungPayCard.getCustomerId());
+    assertNotNull(samsungPayCard.getCustomerLocation());
+    assertNotNull(samsungPayCard.getDebit());
+    assertNotNull(samsungPayCard.isDefault());
+    assertNotNull(samsungPayCard.getDurbinRegulated());
+    assertNotNull(samsungPayCard.getExpirationDate());
+    assertNotNull(samsungPayCard.getExpirationMonth());
+    assertNotNull(samsungPayCard.getExpirationYear());
+    assertNotNull(samsungPayCard.isExpired());
+    assertNotNull(samsungPayCard.getHealthcare());
+    assertNotNull(samsungPayCard.getImageUrl());
+    assertNotNull(samsungPayCard.getIssuingBank());
+    assertNotNull(samsungPayCard.getLast4());
+    assertNotNull(samsungPayCard.getMaskedNumber());
+    assertNotNull(samsungPayCard.getPayroll());
+    assertNotNull(samsungPayCard.getPrepaid());
+    assertNotNull(samsungPayCard.getProductId());
+    assertNotNull(samsungPayCard.getSubscriptions());
+    assertNotNull(samsungPayCard.getToken());
+    assertNotNull(samsungPayCard.getUniqueNumberIdentifier());
+    assertNotNull(samsungPayCard.getUpdatedAt());
 
-    @Test
-    public void searchOnPaymentInstrumentTypeIsSamsungPayCard() {
-        TransactionRequest request = new TransactionRequest().
-            amount(new BigDecimal("1000")).
-            paymentMethodNonce(Nonce.SamsungPayVisa);
+    SamsungPayCard foundSamsungPayCard = (SamsungPayCard) gateway.paymentMethod().find(samsungPayCard.getToken());
+    assertEquals(samsungPayCard.getToken(), foundSamsungPayCard.getToken());
+  }
 
-        Transaction transaction = gateway.transaction().sale(request).getTarget();
+  @Test
+  public void searchOnPaymentInstrumentTypeIsSamsungPayCard() {
+    TransactionRequest request = new TransactionRequest().
+      amount(new BigDecimal("1000")).
+      paymentMethodNonce(Nonce.SamsungPayVisa);
 
-        TransactionSearchRequest searchRequest = new TransactionSearchRequest().
-            id().is(transaction.getId()).
-            paymentInstrumentType().is(PaymentInstrumentType.SAMSUNG_PAY_CARD);
+    Transaction transaction = gateway.transaction().sale(request).getTarget();
 
-        ResourceCollection<Transaction> collection = gateway.transaction().search(searchRequest);
-        assertEquals(PaymentInstrumentType.SAMSUNG_PAY_CARD, collection.getFirst().getPaymentInstrumentType());
+    TransactionSearchRequest searchRequest = new TransactionSearchRequest().
+      id().is(transaction.getId()).
+      paymentInstrumentType().is(PaymentInstrumentType.SAMSUNG_PAY_CARD);
 
-        SamsungPayCardDetails samsungPayCardDetails = collection.getFirst().getSamsungPayCardDetails();
-        assertNotNull(samsungPayCardDetails);
-    }
+    ResourceCollection<Transaction> collection = gateway.transaction().search(searchRequest);
+    assertEquals(PaymentInstrumentType.SAMSUNG_PAY_CARD, collection.getFirst().getPaymentInstrumentType());
 
-    @Test
-    public void createCustomerWithSamsungPayCard() {
-        Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
-        PaymentMethodRequest request = new PaymentMethodRequest().
-            customerId(customer.getId()).
-            paymentMethodNonce(Nonce.SamsungPayVisa);
+    SamsungPayCardDetails samsungPayCardDetails = collection.getFirst().getSamsungPayCardDetails();
+    assertNotNull(samsungPayCardDetails);
+  }
 
-        Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
-        assertTrue(result.isSuccess());
+  @Test
+  public void createCustomerWithSamsungPayCard() {
+    Customer customer = gateway.customer().create(new CustomerRequest()).getTarget();
+    PaymentMethodRequest request = new PaymentMethodRequest().
+      customerId(customer.getId()).
+      paymentMethodNonce(Nonce.SamsungPayVisa);
 
-        Customer foundCustomer = gateway.customer().find(customer.getId());
-        assertEquals(1, foundCustomer.getSamsungPayCards().size());
+    Result<? extends PaymentMethod> result = gateway.paymentMethod().create(request);
+    assertTrue(result.isSuccess());
 
-        SamsungPayCard samsungPayCard = foundCustomer.getSamsungPayCards().get(0);
-        assertTrue(foundCustomer.getPaymentMethods().contains(samsungPayCard));
-    }
+    Customer foundCustomer = gateway.customer().find(customer.getId());
+    assertEquals(1, foundCustomer.getSamsungPayCards().size());
 
-    @Test
-    public void saleWithSamsungPayAndVault() {
-        String samsungPayNonce = Nonce.SamsungPayAmEx;
+    SamsungPayCard samsungPayCard = foundCustomer.getSamsungPayCards().get(0);
+    assertTrue(foundCustomer.getPaymentMethods().contains(samsungPayCard));
+  }
 
-        TransactionRequest request = new TransactionRequest().
-            amount(SandboxValues.TransactionAmount.AUTHORIZE.amount).
-            paymentMethodNonce(samsungPayNonce).
-            options().
-                storeInVault(true).
-                done();
+  @Test
+  public void saleWithSamsungPayAndVault() {
+    String samsungPayNonce = Nonce.SamsungPayAmEx;
 
-        Result<Transaction> result = gateway.transaction().sale(request);
-        assertTrue(result.isSuccess());
-        Transaction transaction = result.getTarget();
+    TransactionRequest request = new TransactionRequest().
+      amount(SandboxValues.TransactionAmount.AUTHORIZE.amount).
+      paymentMethodNonce(samsungPayNonce).
+      options().
+      storeInVault(true).
+      done().
+      creditCard().
+      cardholderName("Samsung Pay Merchant Name").
+      done();
 
-        assertEquals(PaymentInstrumentType.SAMSUNG_PAY_CARD, transaction.getPaymentInstrumentType());
+    Result<Transaction> result = gateway.transaction().sale(request);
+    assertTrue(result.isSuccess());
+    Transaction transaction = result.getTarget();
 
-        SamsungPayCardDetails samsungPayCardDetails = transaction.getSamsungPayCardDetails();
+    assertEquals(PaymentInstrumentType.SAMSUNG_PAY_CARD, transaction.getPaymentInstrumentType());
 
-        assertNotNull(samsungPayCardDetails.getBin());
-        assertNotNull(samsungPayCardDetails.getCardType());
-        assertNotNull(samsungPayCardDetails.getCardholderName());
-        assertNotNull(samsungPayCardDetails.getCommercial());
-        assertNotNull(samsungPayCardDetails.getCountryOfIssuance());
-        assertNotNull(samsungPayCardDetails.getDebit());
-        assertNotNull(samsungPayCardDetails.getDurbinRegulated());
-        assertNotNull(samsungPayCardDetails.getExpirationDate());
-        assertNotNull(samsungPayCardDetails.getExpirationMonth());
-        assertNotNull(samsungPayCardDetails.getExpirationYear());
-        assertNotNull(samsungPayCardDetails.getHealthcare());
-        assertNotNull(samsungPayCardDetails.getImageUrl());
-        assertNotNull(samsungPayCardDetails.getIssuingBank());
-        assertNotNull(samsungPayCardDetails.getLast4());
-        assertNotNull(samsungPayCardDetails.getMaskedNumber());
-        assertNotNull(samsungPayCardDetails.getPayroll());
-        assertNotNull(samsungPayCardDetails.getPrepaid());
-        assertNotNull(samsungPayCardDetails.getProductId());
-        assertNotNull(samsungPayCardDetails.getToken());
-    }
+    SamsungPayCardDetails samsungPayCardDetails = transaction.getSamsungPayCardDetails();
+    assertNotNull(samsungPayCardDetails.getBin());
+    assertNotNull(samsungPayCardDetails.getCardType());
+    assertNotNull(samsungPayCardDetails.getCardholderName());
+    assertNotNull(samsungPayCardDetails.getCommercial());
+    assertNotNull(samsungPayCardDetails.getCountryOfIssuance());
+    assertNotNull(samsungPayCardDetails.getDebit());
+    assertNotNull(samsungPayCardDetails.getDurbinRegulated());
+    assertNotNull(samsungPayCardDetails.getExpirationDate());
+    assertNotNull(samsungPayCardDetails.getExpirationMonth());
+    assertNotNull(samsungPayCardDetails.getExpirationYear());
+    assertNotNull(samsungPayCardDetails.getHealthcare());
+    assertNotNull(samsungPayCardDetails.getImageUrl());
+    assertNotNull(samsungPayCardDetails.getIssuingBank());
+    assertNotNull(samsungPayCardDetails.getLast4());
+    assertNotNull(samsungPayCardDetails.getMaskedNumber());
+    assertNotNull(samsungPayCardDetails.getPayroll());
+    assertNotNull(samsungPayCardDetails.getPrepaid());
+    assertNotNull(samsungPayCardDetails.getProductId());
+    assertNotNull(samsungPayCardDetails.getToken());
+  }
 }
