e0488032f0d50e63a0dc61979e8b99e2627b3c11
Andrew Bissell <andrew.bissell@getbraintree.com>
2020-06-29 23:13:33 +0000

    DTBTCRDSXP-460 Add networkTransactionId to verifications
    Co-authored-by: Andrew Bissell <andrew.bissell@getbraintree.com>
    Co-authored-by: Eric Nicholas <eric.nicholas@getbraintree.com>

--- src/test/java/com/braintreegateway/unittest/CreditCardVerificationTest.java
+++ src/test/java/com/braintreegateway/unittest/CreditCardVerificationTest.java
@@ -1,33 +1,57 @@
 package com.braintreegateway.unittest;
 
+import com.braintreegateway.CreditCardVerification;
+import com.braintreegateway.util.SimpleNodeWrapper;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import com.braintreegateway.CreditCardVerification;
-import com.braintreegateway.util.SimpleNodeWrapper;
-
 public class CreditCardVerificationTest {
-    @Test
-    public void testVerificationParsesNetworkResponseCodeAndText() {
-        String xml = "<verification>"
-                + "    <amount>1.02</amount>"
-                + "    <currency-iso-code>USD</currency-iso-code>"
-                + "    <avs-error-response-code nil=\"true\"></avs-error-response-code>"
-                + "    <avs-postal-code-response-code>I</avs-postal-code-response-code>"
-                + "    <status>processor_declined</status>"
-                + "    <processor-response-code>2000</processor-response-code>"
-                + "    <avs-street-address-response-code>I</avs-street-address-response-code>"
-                + "    <processor-response-text>Do Not Honor</processor-response-text>"
-                + "    <network-response-code>05</network-response-code>"
-                + "    <network-response-text>Do not Honor</network-response-text>"
-                + "    <cvv-response-code>M</cvv-response-code>"
-                + "  </verification>";
+  @Test
+  public void testVerificationParsesNetworkResponseCodeAndText() {
+    String xml = "<verification>"
+                 + "    <amount>1.02</amount>"
+                 + "    <currency-iso-code>USD</currency-iso-code>"
+                 + "    <avs-error-response-code nil=\"true\"></avs-error-response-code>"
+                 + "    <avs-postal-code-response-code>I</avs-postal-code-response-code>"
+                 + "    <status>processor_declined</status>"
+                 + "    <processor-response-code>2000</processor-response-code>"
+                 + "    <avs-street-address-response-code>I</avs-street-address-response-code>"
+                 + "    <processor-response-text>Do Not Honor</processor-response-text>"
+                 + "    <network-response-code>05</network-response-code>"
+                 + "    <network-response-text>Do not Honor</network-response-text>"
+                 + "    <cvv-response-code>M</cvv-response-code>"
+                 + "    <network-transaction-id>ABC1234</network-transaction-id>"
+                 + "  </verification>";
 
-        SimpleNodeWrapper verificationNode = SimpleNodeWrapper.parse(xml);
-        CreditCardVerification verification = new CreditCardVerification(verificationNode);
+    SimpleNodeWrapper verificationNode = SimpleNodeWrapper.parse(xml);
+    CreditCardVerification verification = new CreditCardVerification(verificationNode);
 
-        assertEquals("05", verification.getNetworkResponseCode());
-        assertEquals("Do not Honor", verification.getNetworkResponseText());
-    }
+    assertEquals("05", verification.getNetworkResponseCode());
+    assertEquals("Do not Honor", verification.getNetworkResponseText());
+  }
+
+  @Test
+  public void testVerificationParsesNetworkTransactionId() {
+    String xml = "<verification>"
+                 + "    <amount>1.02</amount>"
+                 + "    <currency-iso-code>USD</currency-iso-code>"
+                 + "    <avs-error-response-code nil=\"true\"></avs-error-response-code>"
+                 + "    <avs-postal-code-response-code>I</avs-postal-code-response-code>"
+                 + "    <status>processor_declined</status>"
+                 + "    <processor-response-code>2000</processor-response-code>"
+                 + "    <avs-street-address-response-code>I</avs-street-address-response-code>"
+                 + "    <processor-response-text>Do Not Honor</processor-response-text>"
+                 + "    <network-response-code>05</network-response-code>"
+                 + "    <network-response-text>Do not Honor</network-response-text>"
+                 + "    <cvv-response-code>M</cvv-response-code>"
+                 + "    <network-transaction-id>ABC1234</network-transaction-id>"
+                 + "  </verification>";
+
+    SimpleNodeWrapper verificationNode = SimpleNodeWrapper.parse(xml);
+    CreditCardVerification verification = new CreditCardVerification(verificationNode);
+
+    assertEquals("ABC1234", verification.getNetworkTransactionId());
+  }
 }
