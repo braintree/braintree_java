75b1509bb6d46161e350733b38f0745bf5f59004
scannillo <samantha.cannillo@getbraintree.com>
2021-05-27 09:46:22 -0500

    Update src/test/java/com/braintreegateway/unittest/CreditCardGatewayTest.java
    Co-authored-by: sestevens <susan.stevens@getbraintree.com>

--- src/test/java/com/braintreegateway/unittest/CreditCardGatewayTest.java
+++ src/test/java/com/braintreegateway/unittest/CreditCardGatewayTest.java
@@ -23,7 +23,7 @@ public class CreditCardGatewayTest {
     }
 
     @Test
-    public void findThrowsErrorOnNullPointer() {
+    public void findThrowsNotFoundExceptionOnNullPointer() {
         CreditCardGateway creditCardGateway = this.gateway.creditCard();
         assertThrows(NotFoundException.class, () -> {
             creditCardGateway.find(null);
