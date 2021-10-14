da462f8346e96c968f43f2bea0fa9f399a783d33
Harshavardhan Ellanki <hellanki@paypal.com>
2021-10-14 04:35:04 +0000

    added plan create/update/show API endpoint

--- src/main/java/com/braintreegateway/PlanGateway.java
+++ src/main/java/com/braintreegateway/PlanGateway.java
@@ -1,28 +1,47 @@
 package com.braintreegateway;
 
-import com.braintreegateway.util.Http;
-import com.braintreegateway.util.NodeWrapper;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.braintreegateway.exceptions.NotFoundException;
+import com.braintreegateway.util.Http;
+import com.braintreegateway.util.NodeWrapper;
+
 public class PlanGateway {
-    private Http http;
-    private Configuration configuration;
+  private Http http;
+  private Configuration configuration;
 
-    public PlanGateway(Http http, Configuration configuration) {
-        this.http = http;
-        this.configuration = configuration;
+  public PlanGateway(Http http, Configuration configuration) {
+    this.http = http;
+    this.configuration = configuration;
+  }
+
+  public List<Plan> all() {
+    NodeWrapper node = http.get(configuration.getMerchantPath() + "/plans");
+
+    List<Plan> plans = new ArrayList<Plan>();
+
+    for (NodeWrapper planResponse : node.findAll("plan")) {
+      plans.add(new Plan(planResponse));
     }
 
-    public List<Plan> all() {
-        NodeWrapper node = http.get(configuration.getMerchantPath() + "/plans");
+    return plans;
+  }
 
-        List<Plan> plans = new ArrayList<Plan>();
+  public Result<Plan> create(PlanRequest request) {
+    NodeWrapper node = http.post(configuration.getMerchantPath() + "/plans", request);
+    return new Result<Plan>(node, Plan.class);
+  }
 
-        for (NodeWrapper planResponse : node.findAll("plan")) {
-            plans.add(new Plan(planResponse));
-        }
-
-        return plans;
+  public Plan find(String id) {
+    if (id == null || id.trim().equals("")) {
+      throw new NotFoundException();
     }
+    return new Plan(http.get(configuration.getMerchantPath() + "/plans/" + id));
+  }
+
+  public Result<Plan> update(String id, PlanRequest request) {
+    NodeWrapper node = http.put(configuration.getMerchantPath() + "/plans/" + id, request);
+    return new Result<Plan>(node, Plan.class);
+  }
 }
--- src/main/java/com/braintreegateway/Result.java
+++ src/main/java/com/braintreegateway/Result.java
@@ -1,104 +1,118 @@
 package com.braintreegateway;
 
-import com.braintreegateway.util.NodeWrapper;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
+import com.braintreegateway.util.NodeWrapper;
+
 public class Result<T> {
 
-    private UsBankAccountVerification usBankAccountVerification;
-    private CreditCardVerification creditCardVerification;
-    private Transaction transaction;
-    private Subscription subscription;
-    private ValidationErrors errors;
-    private Map<String, String> parameters;
-    private String message;
-    private T target;
+  private UsBankAccountVerification usBankAccountVerification;
+  private CreditCardVerification creditCardVerification;
+  private Transaction transaction;
+  private Plan plan;
+  private Subscription subscription;
+  private ValidationErrors errors;
+  private Map<String, String> parameters;
+  private String message;
+  private T target;
 
-    public static <T> T newInstanceFromNode(Class<T> klass, NodeWrapper node) {
-        Throwable cause = null;
-        try {
-            return klass.getConstructor(NodeWrapper.class).newInstance(node);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            cause = e;
-        }
-
-        throw new IllegalArgumentException("Unknown klass: " + klass, cause);
+  public static <T> T newInstanceFromNode(Class<T> klass, NodeWrapper node) {
+    Throwable cause = null;
+    try {
+      return klass.getConstructor(NodeWrapper.class).newInstance(node);
+    } catch (InstantiationException
+        | IllegalAccessException
+        | InvocationTargetException
+        | NoSuchMethodException e) {
+      cause = e;
     }
 
-    public Result() {
+    throw new IllegalArgumentException("Unknown klass: " + klass, cause);
+  }
+
+  public Result() {}
+
+  public Result(ValidationErrors errors) {
+    this.errors = errors;
+  }
+
+  public Result(T target) {
+    this.target = target;
+  }
+
+  public Result(NodeWrapper node, Class<T> klass) {
+    if (node.isSuccess()) {
+      this.target = newInstanceFromNode(klass, node);
+    } else {
+      this.errors = new ValidationErrors(node);
+
+      NodeWrapper usBankAccountVerificationNode = node.findFirst("us-bank-account-verification");
+      if (usBankAccountVerificationNode != null) {
+        this.usBankAccountVerification =
+            new UsBankAccountVerification(usBankAccountVerificationNode);
+      }
+
+      NodeWrapper verificationNode = node.findFirst("verification");
+      if (verificationNode != null) {
+        this.creditCardVerification = new CreditCardVerification(verificationNode);
+      }
+
+      NodeWrapper transactionNode = node.findFirst("transaction");
+      if (transactionNode != null) {
+        this.transaction = new Transaction(transactionNode);
+      }
+
+      NodeWrapper planNode = node.findFirst("plan");
+      if (planNode != null) {
+        this.plan = new Plan(planNode);
+      }
+      NodeWrapper subscriptionNode = node.findFirst("subscription");
+      if (subscriptionNode != null) {
+        this.subscription = new Subscription(subscriptionNode);
+      }
+      this.parameters = node.findFirst("params").getFormParameters();
+      this.message = node.findString("message");
     }
+  }
 
-    public Result(ValidationErrors errors) {
-        this.errors = errors;
-    }
+  public UsBankAccountVerification getUsBankAccountVerification() {
+    return usBankAccountVerification;
+  }
 
-    public Result(T target) {
-        this.target = target;
-    }
+  public CreditCardVerification getCreditCardVerification() {
+    return creditCardVerification;
+  }
 
-    public Result(NodeWrapper node, Class<T> klass) {
-        if (node.isSuccess()) {
-            this.target = newInstanceFromNode(klass, node);
-        } else {
-            this.errors = new ValidationErrors(node);
+  public Plan getPlan() {
+    return plan;
+  }
 
-            NodeWrapper usBankAccountVerificationNode = node.findFirst("us-bank-account-verification");
-            if (usBankAccountVerificationNode != null) {
-                this.usBankAccountVerification = new UsBankAccountVerification(usBankAccountVerificationNode);
-            }
+  public Subscription getSubscription() {
+    return subscription;
+  }
 
-            NodeWrapper verificationNode = node.findFirst("verification");
-            if (verificationNode != null) {
-                this.creditCardVerification = new CreditCardVerification(verificationNode);
-            }
+  public Transaction getTransaction() {
+    return transaction;
+  }
 
-            NodeWrapper transactionNode = node.findFirst("transaction");
-            if (transactionNode != null) {
-                this.transaction = new Transaction(transactionNode);
-            }
-            NodeWrapper subscriptionNode = node.findFirst("subscription");
-            if (subscriptionNode != null) {
-                this.subscription = new Subscription(subscriptionNode);
-            }
-            this.parameters = node.findFirst("params").getFormParameters();
-            this.message = node.findString("message");
-        }
-    }
+  public ValidationErrors getErrors() {
+    return errors;
+  }
 
-    public UsBankAccountVerification getUsBankAccountVerification() {
-        return usBankAccountVerification;
-    }
+  public Map<String, String> getParameters() {
+    return parameters;
+  }
 
-    public CreditCardVerification getCreditCardVerification() {
-        return creditCardVerification;
-    }
+  public T getTarget() {
+    return target;
+  }
 
-    public Subscription getSubscription() {
-        return subscription;
-    }
+  public boolean isSuccess() {
+    return errors == null;
+  }
 
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
-    public ValidationErrors getErrors() {
-        return errors;
-    }
-
-    public Map<String, String> getParameters() {
-        return parameters;
-    }
-
-    public T getTarget() {
-        return target;
-    }
-
-    public boolean isSuccess() {
-        return errors == null;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 }
