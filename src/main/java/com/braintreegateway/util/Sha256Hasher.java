762b7abb69ace713599002a02e0a145b340c41fb
Shaik Saifuddin <sh450956@dal.ca>
2023-04-06 19:27:38 -0300

    created a new class to remove the duplicate code from Sha1 and Sha256 Hasher classes - pull up method refactoring

--- src/main/java/com/braintreegateway/util/Sha256Hasher.java
+++ src/main/java/com/braintreegateway/util/Sha256Hasher.java
@@ -5,31 +5,9 @@ import java.security.MessageDigest;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-public class Sha256Hasher implements Hasher {
+public class Sha256Hasher extends ShaTypeHasher {
 
     public String hmacHash(String privateKey, String content) {
-        String hash = "";
-        try {
-            SecretKeySpec signingKey = new SecretKeySpec(sha256Bytes(privateKey), "SHA-256");
-            Mac mac = Mac.getInstance("HmacSHA256");
-            mac.init(signingKey);
-
-            byte[] rawMac = mac.doFinal(content.getBytes("UTF-8"));
-            byte[] hexBytes = new Hex().encode(rawMac);
-            hash = new String(hexBytes, "ISO-8859-1");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return hash;
-    }
-
-    private byte[] sha256Bytes(String string) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(string.getBytes("UTF-8"));
-            return md.digest();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return super.hmacHash(privateKey, content, "SHA-256");
     }
 }
